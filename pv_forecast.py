84ede0bd7c383d7412fe79ff6395637ab63702f6
lkjhgfdsa2020 <63849650+lkjhgfdsa2020@users.noreply.github.com>
2026-03-05 18:00:26 +0100

    Fix indentation in parameters dictionary

--- pv_forecast.py
+++ pv_forecast.py
@@ -241,7 +241,7 @@ az_open = cfg.azimuth_deg_from_north - 180.0
 # wrap to [-180, 180]
 az_open = ((az_open + 180.0) % 360.0) - 180.0
 
-params = {
+    params = {
         "latitude": cfg.latitude,
         "longitude": cfg.longitude,
         "timezone": cfg.timezone,
